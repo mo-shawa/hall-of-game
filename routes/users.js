4435d28e42ef63cee5646091c6921223bfdb8d1e
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-04-24 17:26:45 -0400

    add user model

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const userCtrl = require('../controllers/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.post('/', userCtrl.create);
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
 
 module.exports = router;
