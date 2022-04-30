93013ff1d15d16236707a67245307c28967c3bd2
Mahmoud El-Shawa <moshawa0@gmail.com>
2022-04-29 22:16:25 -0400

    added comments structure, tested on game - basic implementation

--- controllers/games.js
+++ controllers/games.js
@@ -1,5 +1,6 @@
 const Game = require("../models/game");
 const Item = require("../models/item");
+const Comment = require("../models/comment");
 
 function index(req, res) {
 	Game.find({}, function (err, games) {
@@ -7,20 +8,21 @@ function index(req, res) {
 	});
 }
 
-function show(req, res) {
+async function show(req, res) {
 
-	Item.find({ game: req.params.id })
-		.populate("game").exec(function (err, items) {
-			if (err) return res.send(err.message);
-			res.render('games/view', { items });
+	// Item.find({ game: req.params.id })
+	// 	.populate("game").exec(function (err, items) {
+	// 		if (err) return res.send(err.message);
+	// 		res.render('games/view', { items });
+	// 	});
+	const comments = await Comment.find({ modelID: req.params.id, onModel: "Game" })
+	const rating = comments.reduce((acc, curr) => (acc + curr.rating) / comments.length, 3) / comments.length;
 
-			// Game.findById(req.params.id, function (err, game) {
-			// 	Item.find({ game: game._id }, function (err, items) {
-			// 		console.log(items);
-			// 		res.render("games/view", { game, items });
-			// 	});
-			// });
+	Game.findById(req.params.id, function (err, game) {
+		Item.find({ game: game._id }, function (err, items) {
+			res.render("games/view", { game, items, comments, rating });
 		});
+	});
 }
 
 function newGame(req, res) {
@@ -38,9 +40,21 @@ function create(req, res) {
 	});
 }
 
+const createComment = async (req, res) => {
+	req.body.modelID = req.params.id;
+	req.body.onModel = "Game";
+	const comment = await new Comment(req.body);
+	console.log(comment)
+	await comment.save();
+	res.redirect(`/games/${req.params.id}`);
+
+
+}
+
 module.exports = {
 	index,
 	show,
 	new: newGame,
 	create,
+	createComment
 };
---
+++ models/comment.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    modelID: {
+        type: mongoose.Schema.Types.ObjectId,
+        refPath: 'onModel',
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Item', 'Collection', 'Game'],
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
+
